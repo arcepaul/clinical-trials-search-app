5bece1cdd3e224da0847de8822e042f15c521eaa
Arce, Paul (NIH/NCI) [C] <arcepe@mail.nih.gov>
2019-12-04 12:11:16 -0500

    Update styling on ErrorPage and removed initing string that was flashing on the screen for a split second

--- src/App.js
+++ src/App.js
@@ -8,45 +8,49 @@ import ResultsPage from './views/ResultsPage';
 import TrialDescriptionPage from './views/TrialDescriptionPage';
 import ErrorPage from './views/ErrorPage';
 
-import {useAppInitializer} from './hooks';
+import { useAppInitializer } from './hooks';
 
-const App = ({services, zipConversionEndpoint}) => {
-
+const App = ({ services, zipConversionEndpoint }) => {
   const appHasBeenInitialized = useSelector(
     store => store.globals.appHasBeenInitialized
   );
 
-  const initErrorsList = useSelector(
-    store => store.globals.initErrorsList
-  );
+  const initErrorsList = useSelector(store => store.globals.initErrorsList);
 
   const ctsapiclient = services.ctsSearch();
 
-  useAppInitializer(ctsapiclient, zipConversionEndpoint);  
-
-  
+  useAppInitializer(ctsapiclient, zipConversionEndpoint);
 
   return (
     <>
-    {!appHasBeenInitialized ? 
-      // Is initializaing, show loading screen.
-      (<>initing</>) :
-        initErrorsList.length === 0 ?
-          (<Fragment>
-            <Switch>
-              <Route path="/about-cancer/treatment/clinical-trials/search/r" component={ResultsPage} />
-              <Route path="/about-cancer/treatment/clinical-trials/search/v" component={TrialDescriptionPage} />
-              <Route
-                exact
-                path="/about-cancer/treatment/clinical-trials/search/advanced"
-                render={() => <SearchPage formInit="advanced" />}
-              />
-              <Route path="/about-cancer/treatment/clinical-trials/search/" component={SearchPage} />
-            </Switch>
-          </Fragment>
-          ) :
-          <ErrorPage initErrorsList={initErrorsList} />
-    }
+      {!appHasBeenInitialized ? (
+        // Is initializaing, show loading screen.
+        <></>
+      ) : initErrorsList.length === 0 ? (
+        <Fragment>
+          <Switch>
+            <Route
+              path="/about-cancer/treatment/clinical-trials/search/r"
+              component={ResultsPage}
+            />
+            <Route
+              path="/about-cancer/treatment/clinical-trials/search/v"
+              component={TrialDescriptionPage}
+            />
+            <Route
+              exact
+              path="/about-cancer/treatment/clinical-trials/search/advanced"
+              render={() => <SearchPage formInit="advanced" />}
+            />
+            <Route
+              path="/about-cancer/treatment/clinical-trials/search/"
+              component={SearchPage}
+            />
+          </Switch>
+        </Fragment>
+      ) : (
+        <ErrorPage initErrorsList={initErrorsList} />
+      )}
     </>
   );
 };
--- src/views/ErrorPage/ErrorPage.jsx
+++ src/views/ErrorPage/ErrorPage.jsx
@@ -1,19 +1,41 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { clearForm} from '../../store/actions';
-import {Helmet} from 'react-helmet';
+import { clearForm } from '../../store/actions';
+import { Helmet } from 'react-helmet';
 import { Delighter } from '../../components/atomic';
 import { Link } from 'react-router-dom';
 
 import './ErrorPage.scss';
 
 const ErrorPage = ({ initErrorsList }) => {
-
   const dispatch = useDispatch();
   const formSnapshot = useSelector(store => store.form);
 
   const handleStartOver = () => {
     dispatch(clearForm());
+  };
+
+  const getFieldNameDisplay = fieldName => {
+    const fieldNameMap = {
+      cancerType: 'Cancer Type/Condition',
+      age: 'Age',
+      subtypes: 'Subtypes',
+      stages: 'Stages',
+      findings: 'Findings',
+      keywordPhrases: 'Keywords and Phrases',
+      zip: 'Zip',
+      country: 'Country',
+      healthyVolunteers: 'Healthy Volunteers',
+      trialTypes: 'Trial Type',
+      states: 'States',
+      city: 'City',
+      drugs: 'Drug/Drug Family',
+      treatments: 'Other Treatments',
+      trialId: 'Trial ID',
+      investigator: 'Trial Investigators',
+      leadOrg: 'Lead Organization',
+    };
+    return fieldNameMap[fieldName] || fieldName;
   };
 
   const renderDelighters = () => (
@@ -48,38 +70,42 @@ const ErrorPage = ({ initErrorsList }) => {
         </p>
       </Delighter>
     </div>
-  );  
+  );
 
   return (
     <>
       <Helmet>
         <title>
-          Clinical Trials Search Results - National Cancer Institute
+          Clinical Trials Search - National Cancer Institute
         </title>
-        <meta property="og:title" content="Clinical Trials Search Results" />
-        
+        <meta property="og:title" content="Clinical Trials Search" />
+
         <meta
           name="description"
-          content="Find an NCI-supported clinical trial - Search results"
+          content="Find an NCI-supported clinical trial - Search"
         />
         <meta
           property="og:description"
-          content="Find an NCI-supported clinical trial - Search results"
+          content="Find an NCI-supported clinical trial - Search"
         />
       </Helmet>
-      <article className="results-page">
-        <h1>Clinical Trials Search Results</h1>
+      <article className="error-page">
+        <h1>Clinical Trials Search</h1>
 
-        <div class="results-page__content">
-          <div className="results-page__control --top">
-            <div className="results-page__list">
-              <div class="results-list invalid-zip">
+        <div class="error-page__content">
+          <div className="error-page__control --top">
+            <div className="error-page__list">
+              <div class="error-list">
                 <p>
-                  Sorry you seem to have entered invalid criteria. Please check the
-                  following, and try your search again:
+                  Sorry, you seem to have entered invalid criteria. Please check
+                  the following, and try your search again:
                 </p>
                 <ul>
-                  {initErrorsList.map(item => <li key={item}>{item.fieldName}</li>)}
+                  {initErrorsList.map(item => (
+                    <li key={item}>
+                      {getFieldNameDisplay(item.fieldName)}
+                    </li>
+                  ))}
                 </ul>
                 <p>
                   For assistance, please contact the NCI Contact Center. You can{' '}
@@ -90,26 +116,29 @@ const ErrorPage = ({ initErrorsList }) => {
                 </p>
                 <p>
                   <Link
-                    to={`${formSnapshot.formType === 'basic' ? '/about-cancer/treatment/clinical-trials/search' : '/about-cancer/treatment/clinical-trials/search/advanced'}`}
+                    to={`${
+                      formSnapshot.formType === 'basic'
+                        ? '/about-cancer/treatment/clinical-trials/search'
+                        : '/about-cancer/treatment/clinical-trials/search/advanced'
+                    }`}
                     onClick={handleStartOver}
                   >
                     Try a new search
                   </Link>
                 </p>
               </div>
-              <aside className="results-page__aside --side">
+              <aside className="error-page__aside --side">
                 {renderDelighters()}
               </aside>
             </div>
           </div>
         </div>
-        <aside className="results-page__aside --bottom">
+        <aside className="error-page__aside --bottom">
           {renderDelighters()}
         </aside>
       </article>
     </>
   );
-
-}
+};
 
 export default ErrorPage;
